3a3fed6b3d3652afd507658f614470be679f9716
Ondra Hlaváč <ondra@hlavac.cz>
2026-01-22 17:25:53 +0100

    Add reset README

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: 'AIzaSyCa3MVxUnKJUFZjgNswqm-MyEUxTBh4oUc',
-    authDomain: 'i-work-out-56987.firebaseapp.com',
-    projectId: 'i-work-out-56987',
-    storageBucket: 'i-work-out-56987.appspot.com',
-    messagingSenderId: '924555046683',
-    appId: '1:924555046683:web:860f3f7570118ad43fadbe',
-    measurementId: 'G-MV4ECY6EQZ'
-}
--- src/pages/pw-forget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Layout from '../utils/layout';
-import PasswordForget from '../components/scenes/Auth/scenes/PasswordForget/PasswordForget';
-
-export default () => (
-  <Layout>
-    <PasswordForget />
-  </Layout>
-);
